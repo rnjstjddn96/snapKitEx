65574021c2a1bc412f6e751391cbbcff4b44c626
rnjstjddn96 <rnjstjddn1996@gmail.com>
2020-06-10 13:55:52 +0900

    Initial Commit

---
+++ snapKitPrac/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  snapKitPrac
+//
+//  Created by 권성우 on 2020/06/10.
+//  Copyright © 2020 권성우. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
